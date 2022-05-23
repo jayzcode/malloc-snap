390ba225057b373a033850ba2925b7883a11b7b7
Chandan Kumar <quic_chku@quicinc.com>
2022-05-23 16:16:55 +0530

    genshap.sh cleanup

--- gensnap.sh
+++ gensnap.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-#DEPENDS:
-
 #Remove snap
 rm -f *.snap 2>/dev/null
 
 # Compile and copy the snap
 #CROSS_COMPILE_ARGS="--target-arch=arm64 --enable-experimental-target-arch"
-SNAP_OUT_DIR=$(pwd)/../../../out/
 snapcraft $CROSS_COMPILE_ARGS --destructive-mode
-
-mkdir -p $SNAP_OUT_DIR
-cp -rf *.snap $SNAP_OUT_DIR/
